4b27e446b40f1127804a5a78c04eb82cd03c5397
maheshwarligade <maheshwar.ligade@gmail.com>
2021-01-02 14:17:48 +0530

    changes in pointer

--- goPointer.go
+++ goPointer.go
@@ -6,11 +6,12 @@ func main() {
 	k, l := 24, 8086
 
 	p := &k          // point to k
-	fmt.Println(*p) // read k through the pointer
+	fmt.Println("Value of k from pointer = ",*p) // read k through the pointer
+	fmt.Println("Value of P ",p) // value of p
 	*p = 29         // set k through the pointer
-	fmt.Println(k)  // see the new value of k
+	fmt.Println("Value of k = ",k)  // see the new value of k
 
 	p = &l         // point to l
 	*p = *p / 17   // divide l through the pointer
-	fmt.Println(l) // see the n ew value of l
+	fmt.Println("Value of l = ",l) // see the n ew value of l
 }
