f7107095110aed66ff3a747568a1358769977eab
maheshwarligade <maheshwar.ligade@gmail.com>
2021-01-04 07:17:01 +0530

    higher order function changes

--- funcAsParam.go
+++ funcAsParam.go
@@ -20,10 +20,43 @@ func apply(x int, y int, f func(int,int) int) int {
     r := f(x,y)
     return r
 }
+// higher order function to return function 
+func getAddSub() (func(int, int) int, func(int, int) int) {
+
+    add := func(x, y int) int {
+        return x + y
+    }
+
+    sub := func(x, y int) int {
+        return x - y
+    }
+
+    return add, sub
+}
+
+func applyReturn(x, y int, add func(int, int) int, sub func(int, int) int) (int, int) {
+
+    r1 := add(x, y)
+    r2 := sub(x, y)
+
+    return r1, r2
+}
 
 func main() {
-    r1 := apply(3,2, add)
-    r2 := apply(3,2, sub)
+
+    x := 3
+    y := 2
+
+    r1 := apply(x,y, add)
+    r2 := apply(x,y, sub)
     fmt.Println(r1)
     fmt.Println(r2)
+
+    // return function
+    add, sub := getAddSub()
+    fmt.Println("function as a return type")
+    re1, re2 := applyReturn(x, y, add, sub)
+
+    fmt.Printf("%d + %d = %d\n", x, y, re1)
+    fmt.Printf("%d - %d = %d\n", x, y, re2)
 }
