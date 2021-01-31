2bdde31715c8e1710d70c778585cc590fde1283d
maheshwarligade <maheshwar.ligade@gmail.com>
2021-01-31 08:40:41 +0530

    ärithematic operator

---
+++ arithematcisop.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var y = 6
+    var x = 7
+    var z = 8
+
+    var add = y + x + z
+    var sb = z - y
+    var mult = y * x
+    var div = z / 3
+    var rem = z % y
+
+    fmt.Println(add)
+    fmt.Println(sb)
+    fmt.Println(mult)
+    fmt.Println(div)
+	fmt.Println(rem)
+	
+
+}
